d592788ed3c41a816ac0d84310e20759619516bb
cristiano <cristiano.souza.mendonca@gmail.com>
2014-12-31 15:47:09 -0200

    Initial commit

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,40 @@
+# This is a manifest file that'll be compiled into application.js, which will include all the files
+# listed below.
+#
+# Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+# or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+#
+# It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+# compiled file.
+#
+# Read Sprockets README (https:#github.com/sstephenson/sprockets#sprockets-directives) for details
+# about supported directives.
+#
+#= require turbolinks
+#= require js/jquery.min
+#= require js/jquery-migrate-1.2.1.min
+#= require js/jquery-ui
+#= require js/bootstrap
+#= require js/jquery.parallax
+#= require js/jquery.wait
+#= require js/modernizr-2.6.2.min
+#= require js/modernizr.custom
+#= require js/revolution-slider/js/jquery.themepunch.tools.min
+#= require js/revolution-slider/js/jquery.themepunch.revolution.min
+#= require js/jquery.nivo.slider.pack
+#= require js/jquery.prettyPhoto
+#= require js/superfish
+#= require js/tweetMachine
+#= require js/tytabs
+#= require js/jquery.gmap.min
+#= require js/circularnav
+#= require js/jquery.sticky
+#= require js/jflickrfeed
+#= require js/imagesloaded.pkgd.min
+#= require js/waypoints.min
+#= require js/custom
+#= require jquery.mask
+#= require_tree .
+
+jQuery ->
+  $('.telefone').mask('(00) 0000-00009')
---
+++ config/initializers/assets.rb
@@ -0,0 +1,46 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( img/ico/favicon.ico
+                                                  img/ico/apple-touch-icon.png
+                                                  img/ico/apple-touch-icon-72.png
+                                                  img/ico/apple-touch-icon-114.png
+                                                  img/ico/apple-touch-icon-144.png
+                                                  img/logo.png
+                                                  img/slider/slider-bg-1.jpg
+                                                  img/slider/responsive-iphone.png
+                                                  img/slider/responsive-ipad.png
+                                                  img/slider/responsive-imac.png
+                                                  img/slider/slider-bg-2.jpg
+                                                  img/iphone5c.png
+                                                  img/portfolio/portfolio-1.jpg
+                                                  img/portfolio/portfolio-2.jpg
+                                                  img/portfolio/portfolio-3.jpg
+                                                  img/portfolio/portfolio-4.jpg
+                                                  img/portfolio/portfolio-5.jpg
+                                                  img/portfolio/portfolio-6.jpg
+                                                  img/clientslogo/logo-2.png
+                                                  img/portfolio/portfolio-7.jpg
+                                                  img/clientslogo/logo-1.png
+                                                  img/clientslogo/logo-3.png
+                                                  img/clientslogo/logo-4.png
+                                                  img/clientslogo/logo-5.png
+                                                  img/testimonial/team-member-1.jpg
+                                                  img/testimonial/team-member-2.jpg
+                                                  img/testimonial/team-member-3.jpg
+                                                  css/bootstrap.css
+                                                  css/bootstrap-theme.css
+                                                  css/fonts/font-awesome/css/font-awesome.css
+                                                  css/animations.css
+                                                  css/superfish.css
+                                                  css/revolution-slider/css/settings.css
+                                                  css/prettyPhoto.css
+                                                  css/style.css
+                                                  css/global.css
+                                                  css/colors/blue.css
+                                                  css/theme-responsive.css )
