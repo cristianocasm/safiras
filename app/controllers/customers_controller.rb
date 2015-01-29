d78de18c1c3893e5882991b13a63599cf0fcc242
cristiano <cristiano.souza.mendonca@gmail.com>
2015-01-29 20:31:24 -0200

    Finalizando

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,26 @@
+class CustomersController < ApplicationController
+  def new
+    logger.info "***AGENT: #{request.user_agent}"
+    @customer = Customer.new
+  end
+
+  # POST /customers
+  # POST /customers.json
+  def create
+    @customer = Customer.new(customer_params)
+
+    respond_to do |format|
+      if @customer.save
+        finished("prices", reset: false)
+      end
+      format.js { render "modal_response" }
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def customer_params
+      params.require(:customer).permit(:nome, :email, :telefone, :dispositivo)
+    end
+end
