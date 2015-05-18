52025f86d52711bac0a3bda6ea3ab66dbbaa537a
cristiano <cristiano.souza.mendonca@gmail.com>
2015-05-18 12:50:04 -0300

    Gerando versão que redireciona cliente para meu.safirasaloes.com.br

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,7 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include Capybara::DSL
+  include Devise::TestHelpers
 
   # Add more helper methods to be used by all tests here...
   reporter_options = { color: true, slow_count: 5 }
